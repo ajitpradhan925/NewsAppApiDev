e0118555d81ebd10d416334ba7b3cf614f7cd303
Ajit Pradhan <ajitpradhan925@gmail.com>
2021-08-31 07:57:01 +0530

    Register User API, Setup Nodemailer and JWT Token

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const userRoute = require('./routes/userRoute');
+
+require('dotenv').config();
+require('colors');
+
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/users', userRoute);
+
+app.get('*', function (req, res) {
+    console.log('Enpoint does not exist.');
+    res.status(404).send('Enpoint does not exist.');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+    PORT,
+    console.log(`Server is connected in ${process.env.NODE_ENV} mode on port ${PORT}`.red)
+);
---
+++ utils/Mailer.js
@@ -0,0 +1,37 @@
+var _ = require('lodash');
+
+const nodemailer = require('nodemailer');
+
+
+var config = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'codewithajit925@gmail.com',
+        pass: 'Ajit@123'
+    }
+};
+
+
+var transporter = nodemailer.createTransport(config);
+
+var defaultMail = {
+    from: 'codewithajit925@gmail.com',
+    text: 'test test'
+}
+
+const send = (to, subject, html) => {
+
+    // Use default setting
+    mail = _.merge({html}, defaultMail, to);
+
+    transporter.sendMail(mail, function(error, info) {
+        if (error) return console.log(error);
+        console.log('mail sent', info.response);
+    });
+}
+
+
+module.exports = {
+    send
+}
