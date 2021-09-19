f53fbf1f8614f4e5b6096f851bbd62575e21aaac
Ajit Pradhan <ajitpradhan925@gmail.com>
2021-09-19 20:08:32 +0530

    Code Modified

--- utils/Mailer.js
+++ utils/Mailer.js
@@ -1,38 +1,33 @@
-var _ = require('lodash');
-
-const nodemailer = require('nodemailer');
-
+var _ = require('lodash');	
+var nodemailer = require('nodemailer');
 
 var config = {
     host: 'smtp.gmail.com',
     port: 587,
     auth: {
-        user: 'codewithajit925@gmail.com',
-        pass: 'Ajit@123'
+        user: 'ajitpradhan925@gmail.com',
+        pass: 'guopulchtjivpsat'
     }
 };
-
-
+    
 var transporter = nodemailer.createTransport(config);
 
 var defaultMail = {
-    from: 'codewithajit925@gmail.com',
-    pass: 'Ajit@123',
-    text: 'test1 test'
-}
+    from: 'ajitteaches@gmail.com',
+    text: 'test text',
+};
+
 
 const send = (to, subject, html) => {
-
-    // Use default setting
+    // use default setting
     mail = _.merge({html}, defaultMail, to);
-
-    transporter.sendMail(mail, function(error, info) {
-        if (error) return console.log(error);
-        console.log('mail sent', info.response);
+    
+    // send email
+    transporter.sendMail(mail, function(error, info){
+        if(error) return console.log(error);
+        console.log('mail sent:', info.response);
     });
 }
-
-
 module.exports = {
     send
 }
